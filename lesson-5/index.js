d86056b3151a5733a28eac3103f0fe05da645efe
anyutask3 <anyutask3@gmail.com>
2023-04-03 15:33:11 +0400

    "Add Lesson 5 homework"

---
+++ lesson-5/index.js
@@ -0,0 +1,23 @@
+const firstName = 'Seda';
+if(firstName === 'Seda'){
+    console.log('She can help you');
+} else {
+    console.log('The other may be helpfull too');
+};
+
+function zuygKamKent(num){
+//num % 2 == 1  -- cuyc e talis bajanman ardyunqum mnacac ardyunq@
+    if (num % 2 === 0){
+        return 'Zuyg';
+    } else {
+        return 'Kent';
+    }
+};
+console.log(zuygKamKent(-303));
+
+// ''; //- datark string (fasle arjeq)
+// 0 // false arjeq
+// [] // false arjeq
+// {} // false arjec
+// null // false arjec
+// undifind // false arjec
