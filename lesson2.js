5e0aa16fb71cd5df16dc1fde7cf86ce861c9ff9f
Ani <95679333+mariammeni1@users.noreply.github.com>
2023-04-08 20:39:55 +0400

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,15 @@
+
+function getGender(person) {
+    if (person.gender === 'female' || person.gender === 'male') {
+     return 'adekvat';
+    } else {
+        return 'voch adekvat';
+    };
+
+};
+         console.log(getGender());
+
+
+
+ 
+ 
